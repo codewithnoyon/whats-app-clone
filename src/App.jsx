2dbf65abcbca4fbcc5e362135a89f79476d644cd
codernoyonofficial <codernoyon@gmail.com>
2022-10-02 15:51:05 +0600

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login/Login'
 
 function App() {
 
   return (
-    <Login/>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+    </Routes>
   )
 }
 
